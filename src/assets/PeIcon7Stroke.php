5121332c1bb953e969c0e5bb62477dc925b2c2fb
Andrey Kuzmenko <andrey.kuzmenko.job@gmail.com>
2016-11-25 17:20:58 +0500

    save

--- src/assets/PeIcon7Stroke.php
+++ src/assets/PeIcon7Stroke.php
@@ -14,24 +14,25 @@ class PeIcon7Stroke extends AssetBundle
      * @inherit
      */
     public $sourcePath = '@vendor/rey-pro/yii2-addons/dist/pe-icon-7-stroke';
-    
+
     /**
      * @inherit
      */
     public $css = [
         'css/pe-icon-7-stroke.css',
         'css/helper.css',
-];
+    ];
+
     /**
      * Initializes the bundle.
      * Set publish options to copy only necessary files (in this case css and font folders)
      * @codeCoverageIgnore
      */
     public function init()
-{
-    parent::init();
-    $this->publishOptions['beforeCopy'] = function ($from, $to) {
-        return preg_match('%(/|\\\\)(fonts|css)%', $from);
-    };
-}
+    {
+        parent::init();
+        $this->publishOptions['beforeCopy'] = function ($from, $to) {
+            return preg_match('%(/|\\\\)(fonts|css)%', $from);
+        };
+    }
 }
