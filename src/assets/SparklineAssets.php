dcc9a4d78811e403802e2401a06df2779d91a74b
Andrey Kuzmenko <andrey.kuzmenko.job@gmail.com>
2016-11-28 11:34:30 +0500

    save

---
+++ src/assets/SparklineAssets.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link https://github.com/rey-pro/yii2-addons
+ * @copyright Copyright (c) 2016 rey-pro
+ * @license https://github.com/rey-pro/yii2-addons/blob/master/LICENSE
+ */
+namespace rey\addons\assets;
+use yii\web\AssetBundle;
+
+class SparklineAssets extends AssetBundle
+{
+    // The files are not web directory accessible, therefore we need
+    // to specify the sourcePath property. Notice the @bower alias used.
+    public $sourcePath = '@bower/bower-jquery-sparkline/dist';
+
+    public $js=[
+        'jquery.sparkline.retina.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+}
